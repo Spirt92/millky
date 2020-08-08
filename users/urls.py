2330124abd05c33763adfef07b94d82760b85e2d
spirt92 <spirt92@ukr.net>
2020-08-08 15:57:28 +0300

    User authentication(url fixes)

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,5 @@
-from django.contrib.auth import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='login'),
+    path('users/', include('django.contrib.auth.urls')),
 ]
